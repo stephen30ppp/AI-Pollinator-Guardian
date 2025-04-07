623d221b4ec9a564f32e339d5cf64e2f1628db35
Joan-yy <joanshum0423@gmail.com>
2025-04-07 22:26:48 +0800

    PollinatorIdentification

--- ai_pollinator_guardian/lib/main.dart
+++ ai_pollinator_guardian/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:ai_pollinator_guardian/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'app.dart';
-import 'services/firebase_service.dart';
+
+import 'package:firebase_vertexai/firebase_vertexai.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,6 +12,13 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+
+  // Initialize the Vertex AI service and create a `GenerativeModel` instance
+// Specify a model that supports your use case
+final model =
+      FirebaseVertexAI.instance.generativeModel(model: 'gemini-2.0-flash');
+
 
   runApp(
     const PollinatorGuardianApp()
